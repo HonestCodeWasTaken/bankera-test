c326d61fae368ee784c96412a8dce78e8aeb2360
Tomas Radionovas <Tomas.Radionovas@ktu.edu>
2022-02-19 00:05:33 +0200

    calculate currency, fixed async issue with setstate, used a callback to original state i think

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,9 @@ import type { NextPage } from 'next'
 import {
   FormControl,
   FormLabel,
-  FormErrorMessage,
   FormHelperText,
-  Center,
   Input,
   Flex,
-  Stack,
-  Skeleton,
   Spinner,
   Button,
   HStack,
@@ -17,29 +13,36 @@ import {
 } from '@chakra-ui/react'
 import { useEffect, useState } from 'react'
 import ApiCalls from '../Utils/ApiCalls'
-import { IBtcData, IExchangeData } from '../Interfaces/IBtcData'
+import { IBtcData, ICurrency, IExchangeData } from '../Interfaces/IBtcData'
 import CurrencyField from '../components/CurrencyField'
 import {
   Menu,
   MenuButton,
   MenuList,
-  MenuItem,
   MenuItemOption,
-  MenuGroup,
   MenuOptionGroup,
-  MenuDivider,
 } from '@chakra-ui/react'
 
 const Home: NextPage = () => {
-  const [currentBtcData, setCurrentBtcData] = useState<IBtcData>()
-  const [currentFields, setCurrentFields] = useState<Array<IExchangeData>>()
-  const [currentInitFields, setCurrentInitFields] = useState<Array<IExchangeData>>()
+  const [disclaimer, setDisclaimer] = useState<string>('')
+  const [numberInput, setNumberInput] = useState<string>('1')
+  const [currentFields, setCurrentFields] = useState<Array<IExchangeData>>([])
+  const [currentInitFields, setCurrentInitFields] = useState<Array<IExchangeData>>([])
   const MINUTE_MS = 60000;
   const dataFetch = async () => {
     const data: IBtcData = await ApiCalls.fetchUrl('https://api.coindesk.com/v1/bpi/currentprice.json')
-    setCurrentBtcData(data)
+    let fields: Array<IExchangeData> = Object.values(data.bpi)
+    console.log(fields[1].rate_float)
+    setCurrentInitFields(fields)
+    // if (currentFields) {
+      setCurrentFields(prevValue => fields.filter(x => prevValue.some(y => y.code === x.code)))
+      //? if you need to set state based on the previous value of the state,
+      //? you should use the callback version to make sure you're working with the correct previous state
+      // let value = fields.filter(x => currentFields.some(y => y.code === x.code))
+      // console.log(value[0].rate_float)
+      // setCurrentFields(value)
+    // }
   }
-  
   const removeField = (i: number) => {
     if (currentFields) {
       let fieldsCopy = currentFields
@@ -47,57 +50,46 @@ const Home: NextPage = () => {
       setCurrentFields(fieldsCopy)
     }
   }
-  const addField = (i: number) => {
-    if (currentFields) {
-      let currencyToPush = currentFields[i]
-      let pushedFields = currentFields
-      pushedFields.push(currencyToPush)
-      setCurrentFields(pushedFields)
-    }
-  }
   const onSelectChange = (code: string | string[]) => {
     if (currentInitFields !== undefined) {
-          let newArray : Array<IExchangeData> = currentInitFields.filter(x => code.includes(x.code))
-          console.log(newArray)
-          setCurrentFields(newArray)
+      const newArray: Array<IExchangeData> = currentInitFields.filter(x => code.includes(x.code))
+      console.log(newArray)
+      setCurrentFields(newArray)
     }
-
-    // if (currentInitFields && Array.isArray(code)) {
-    //   for (let index = 0; index < code.length; index++) {
-    //     const element = code[index];
-    //     const newCurrentField = currentInitFields.find(x => x.code === element)
-    //     if (newCurrentField !== undefined) {
-    //       newArray.push(newCurrentField)
-    //     }
-    //   }
-    //   setCurrentFields(newArray)
-    // }
+  }
+  const dataFetchInit = async () => {
+    const data: IBtcData = await ApiCalls.fetchUrl('https://api.coindesk.com/v1/bpi/currentprice.json')
+    setDisclaimer(data.disclaimer)
+    const values = Object.values(data.bpi)
+    setCurrentFields(values)
+    setCurrentInitFields(values)
   }
   useEffect(() => {
-    const dataFetchInit = async () => {
-      const data: IBtcData = await ApiCalls.fetchUrl('https://api.coindesk.com/v1/bpi/currentprice.json')
-      setCurrentBtcData(data)
-      setCurrentFields(Object.values(data.bpi))
-      setCurrentInitFields(Object.values(data.bpi))
-    }
     dataFetchInit()
   }, [])
 
+  
   useEffect(() => {
+    // dataFetchInit()
     const interval = setInterval(() => {
       dataFetch()
     }, MINUTE_MS);
-    return () => clearInterval(interval); // This represents the unmount function, in which you need to clear your interval to prevent memory leaks.
+    return () => {
+      console.log('component unmounted')
+      clearInterval(interval)
+     } // This represents the unmount function, in which you need to clear your interval to prevent memory leaks.
   }, [])
 
 
   return (
+    <>
+    
     <Flex flexDir="column" minHeight="100vh" alignItems='center' justifyContent="center">
-      <Flex flexDir="column" maxW='400px' maxH='400px'>
+      <Flex px={3} flexDir="column" maxW='400px' maxH='400px'>
         <FormControl py={3}>
           <FormLabel >Bitcoin converter</FormLabel>
-          <Input defaultValue={1} id='email' type='number' />
-          <FormHelperText>{currentBtcData?.disclaimer}</FormHelperText>
+          <Input value={numberInput} onChange={(event: React.ChangeEvent<HTMLInputElement>) => setNumberInput(event.target.value)} defaultValue={1} type='number' />
+          <FormHelperText>{disclaimer}</FormHelperText>
         </FormControl>
         <HStack py={2}>
           <Heading py={2} as='h5' fontSize='22px'>Currency exchange rates</Heading>
@@ -117,11 +109,12 @@ const Home: NextPage = () => {
         </HStack>
         {currentFields ? <Flex flexDir="column">
           {currentFields.map((currency, index) => (
-            <CurrencyField key={index} currency={currency} removeCurrency={() => removeField(index)}></CurrencyField>
+            <CurrencyField key={index} currency={currency} removeCurrency={() => removeField(index)} numberInput={numberInput}></CurrencyField>
           ))}
         </Flex> : <Spinner />}
       </Flex>
     </Flex >
+    </>
   )
 }
 
